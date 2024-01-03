3148f7e1e7beeaca780390247831201fdcd08fe2
bbeda <bogdan.beda@force24.co.uk>
2024-01-03 10:02:52 +0200

    - minor work

---
+++ src/Minerva/Minerva.WebApp/Minerva.WebApp/Components/TaskItems/List/TaskItemListItemModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Minerva.WebApp.Components.TaskItems.List;
+
+public class TaskItemListItemModel
+{
+    public required string Id { get; init; }
+
+    public required string Title { get; init; }
+
+    public string? Description { get; init; }
+
+    public DateOnly? DueDate { get; init; }
+
+    public required DateTimeOffset CreatedOn { get; init; }
+}
