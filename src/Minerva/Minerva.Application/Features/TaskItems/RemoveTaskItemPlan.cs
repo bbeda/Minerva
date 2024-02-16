faacdc161d184fb99e94a697f2f849ac30e7e87b
Bogdan <bogdan.beda@force24.co.uk>
2024-02-16 18:04:49 +0200

    - started work on planning removal

---
+++ src/Minerva/Minerva.Application/Features/TaskItems/RemoveTaskItemPlan.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Minerva.Application.Common;
+
+namespace Minerva.Application.Features.TaskItems;
+public class RemoveTaskItemPlanCommand : IRequest<CommandResult<TaskItemPlanning>>
+{
+    public Guid TaskId { get; init; }
+
+    public TaskItemPlanningType Types { get; init; }
+}
+
+internal class RemoveTaskItemPlanCommandHandler : IRequestHandler<RemoveTaskItemPlanCommand, CommandResult<TaskItemPlanning>>
+{
+    public Task<CommandResult<TaskItemPlanning>> Handle(RemoveTaskItemPlanCommand request, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
